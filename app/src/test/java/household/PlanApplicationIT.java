c0a5f190ce198a9a269057f963c0e270f40e558e
André Schreck <andre.schreck@gmail.com>
2017-06-06 22:09:08 +0200

    test

--- app/src/test/java/household/PlanApplicationIT.java
+++ app/src/test/java/household/PlanApplicationIT.java
@@ -1,6 +1,7 @@
 package household;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import org.junit.Test;
@@ -34,11 +35,23 @@ public class PlanApplicationIT {
 		//.with(httpBasic("user", "Zb&VWN-~;PWh")
 		//.content("{\"email\":\"a@bc.de\", \"password\":\"12345asdf\"}")
 		
-		MockHttpServletResponse response = mvc.perform(post("/api/households").contentType("application/vnd.household.v1+json"))
+		MockHttpServletResponse postResponse = mvc.perform(post("/api/households").contentType("application/vnd.household.v1+json"))
 		        .andReturn()
 		        .getResponse();
+
+		assertThat(postResponse.getStatus()).isEqualTo(200);
 		
-		assertThat(response.getStatus()).isEqualTo(200);
+		String postContent = postResponse.getContentAsString();
+		
+		MockHttpServletResponse getResponse = mvc.perform(get("/api/households/1").accept("application/vnd.household.v1+json"))
+				.andReturn()
+				.getResponse();
+		
+		assertThat(getResponse.getStatus()).isEqualTo(200);
+		
+		String getContent = getResponse.getContentAsString();
+		
+		assertThat(postContent).isEqualTo(getContent);
 		
 //		User existingUser = userRepository.determineUser("a@bc.de");
 		
