08169bec6de54539cd8ae4b4c6d1d02b697827ee
André Schreck <andre.schreck@gmail.com>
2019-01-20 12:55:56 +0100

    inner routing

--- web/src/main/web/src/app/app-routing.module.ts
+++ web/src/main/web/src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HouseholdPageComponent } from './household-page/household-page.component';
 import { Page } from './page.enum';
-import {AuthGuardService} from './auth-guard.service';
-import {ShoppingListPageComponent} from './shopping-list-page/shopping-list-page.component';
-import {CleaningPlanPageComponent} from './cleaning-plan-page/cleaning-plan-page.component';
-import {FoodPlanPageComponent} from './food-plan-page/food-plan-page.component';
-import {RecipesPageComponent} from './recipes-page/recipes-page.component';
-import {HouseholdResolverService} from './household-page/household-resolver.service';
+import { AuthGuardService} from './auth-guard.service';
+import { ShoppingListPageComponent } from './shopping-list-page/shopping-list-page.component';
+import { CleaningPlanPageComponent } from './cleaning-plan-page/cleaning-plan-page.component';
+import { FoodPlanPageComponent } from './food-plan-page/food-plan-page.component';
+import { RecipesPageComponent } from './recipes-page/recipes-page.component';
+import { HouseholdResolverService } from './household-page/household-resolver.service';
 
 const routes: Routes = [{
     path: '',
@@ -23,27 +23,34 @@ const routes: Routes = [{
     canActivate: [AuthGuardService],
     resolve: [
         HouseholdResolverService
+    ],
+    children: [
+        {
+            path: Page.ShoppingList,
+            component: ShoppingListPageComponent,
+            canActivate: [AuthGuardService],
+            outlet: 'inner'
+        }, {
+            path: Page.CleaningPlan,
+            component: CleaningPlanPageComponent,
+            canActivate: [AuthGuardService],
+            outlet: 'inner'
+        }, {
+            path: Page.FoodPlan,
+            component: FoodPlanPageComponent,
+            canActivate: [AuthGuardService],
+            outlet: 'inner'
+        }, {
+            path: Page.Recipes,
+            component: RecipesPageComponent,
+            canActivate: [AuthGuardService],
+            outlet: 'inner'
+        }
     ]
-}, {
-    path: Page.ShoppingList,
-    component: ShoppingListPageComponent,
-    canActivate: [AuthGuardService]
-}, {
-    path: Page.CleaningPlan,
-    component: CleaningPlanPageComponent,
-    canActivate: [AuthGuardService]
-}, {
-    path: Page.FoodPlan,
-    component: FoodPlanPageComponent,
-    canActivate: [AuthGuardService]
-}, {
-    path: Page.Recipes,
-    component: RecipesPageComponent,
-    canActivate: [AuthGuardService]
 }];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, {useHash: true})],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
