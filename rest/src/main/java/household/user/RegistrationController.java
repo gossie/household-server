8a6b3a9f84d9bc5475dd15017477b46edc8f79d4
André Schreck <andre.schreck@gmail.com>
2019-10-20 16:25:26 +0200

    issue #4: restructured views

--- rest/src/main/java/household/user/RegistrationController.java
+++ rest/src/main/java/household/user/RegistrationController.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,13 +23,14 @@ public class RegistrationController {
 	private final UserService userService;
 
 	@PostMapping
-	public String createUser(@RequestBody MultiValueMap<String, String> data) {
+	public String createUser(@RequestBody MultiValueMap<String, String> data, Model model) {
 		if (!Objects.equals(data.getFirst("password"), data.getFirst("passwordRepeat"))) {
 			throw new BadRequestException();
 		}
 
 	    try {
             userService.createUser(new User(null, ((String) data.getFirst("email")).toLowerCase(), ((String) data.getFirst("password"))));
+            model.addAttribute("successMessage", "Benutzer wurde angelegt");
             return "login";
         } catch(UserAlreadyExistsException e) {
             throw new ConflictException(e);
