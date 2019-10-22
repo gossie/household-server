e7e8584e5c0c08011bd80f11d9bc7d3cc5d1ef7b
André Schreck <andre.schreck@gmail.com>
2019-10-22 23:57:01 +0200

    issue #4: enable reload

--- app/src/main/java/household/MvcConfig.java
+++ app/src/main/java/household/MvcConfig.java
@@ -11,10 +11,10 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/index").setViewName("index");
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/registration").setViewName("registration");
-        registry.addViewController("/app").setViewName("forward:/app.html");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/registration.html").setViewName("registration");
+        registry.addViewController("/household.html").setViewName("forward:/app.html");
     }
 
     @Override
--- web/src/main/web/src/app/app-routing.module.ts
+++ web/src/main/web/src/app/app-routing.module.ts
@@ -10,27 +10,34 @@ const routes: Routes = [{
     {
         path: Page.Cover,
         loadChildren: () => import('./household-page/cover-page/cover.module').then(m => m.CoverModule),
+        pathMatch: 'full'
     },
     {
         path: Page.ShoppingList,
         loadChildren: () => import('./household-page/shopping-list-page/shopping-list.module').then(m => m.ShoppingListModule),
+        pathMatch: 'full'
     },
     {
         path: Page.CleaningPlan,
         loadChildren: () => import('./household-page/cleaning-plan-page/cleaning-plan.module').then(m => m.CleaningPlanModule),
+        pathMatch: 'full'
     },
     {
         path: Page.FoodPlan,
         loadChildren: () => import('./household-page/food-plan-page/food-plan.module').then(m => m.FoodPlanModule),
+        pathMatch: 'full'
     },
     {
         path: Page.Cookbook,
         loadChildren: () => import('./household-page/cookbook-page/cookbook.module').then(m => m.CookbookModule),
+        pathMatch: 'full'
     }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    imports: [RouterModule.forRoot(routes, {
+        useHash: true
+    })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
