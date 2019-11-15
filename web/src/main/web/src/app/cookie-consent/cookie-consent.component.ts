7b2913d942d3cda3733bd7bda6f045dc1fc92007
André Schreck <andre.schreck@gmail.com>
2019-11-15 17:37:05 +0100

    issue #36: cookie banner

--- web/src/main/web/src/app/cookie-consent/cookie-consent.component.ts
+++ web/src/main/web/src/app/cookie-consent/cookie-consent.component.ts
@@ -23,7 +23,7 @@ export class CookieConsentComponent {
         for (let i = 0; i < ca.length; i++) {
             let c = ca[i];
             while (c.charAt(0) === ' ') {
-            c = c.substring(1);
+                c = c.substring(1);
             }
             if (c.indexOf(name) === 0) {
                 return c.substring(name.length, c.length);
