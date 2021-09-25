ffc4e27421da84775ffed71d276b3d5e13261e8e
André Schreck <andre.schreck@gmail.com>
2021-09-25 23:01:21 +0200

    remove hateoas annotation

--- app/src/main/java/household/PlanApplication.java
+++ app/src/main/java/household/PlanApplication.java
@@ -3,12 +3,9 @@ package household;
 import com.google.common.eventbus.EventBus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 
-@SpringBootApplication(exclude = HypermediaAutoConfiguration.class)
-@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@SpringBootApplication
 public class PlanApplication {
 
 	public static void main(String[] args) {
--- shopping-list/shopping-list-rest/src/test/java/household/shoppinglist/TestApplication.java
+++ shopping-list/shopping-list-rest/src/test/java/household/shoppinglist/TestApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
-@SpringBootApplication(exclude = HypermediaAutoConfiguration.class)
-@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@SpringBootApplication
 public class TestApplication {
 
     public static void main(String[] args) {
