4d9889020e5ce20b85d6c62990de56ac2c22325a
André Schreck <moin@andreschreck.de>
2023-01-06 10:34:39 +0100

    #69: revert

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { inject, NgModule } from '@angular/core';
-import { Routes, RouterModule, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { Page } from './page.enum';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 const routes: Routes = [{
         path: '',
-        canActivate: [(_: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+        canActivate: [() => {
             const router = inject(Router);
             const jwt = localStorage.getItem('jwt');
             if (jwt) {
-                return router.parseUrl(state.url);
+                return router.parseUrl(`/${Page.Household}/${Page.Cover}`)
             } else {
-                return router.parseUrl(`/${Page.Login}`);
+                return router.parseUrl(`/${Page.Login}`)
             }
         }],
         component: LoginPageComponent,
